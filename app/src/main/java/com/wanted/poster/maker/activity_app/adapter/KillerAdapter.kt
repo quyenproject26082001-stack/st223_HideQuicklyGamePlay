1a75beedc1dcde965fd00e79d9727262e72c86c0
unknown <quyenle26082001@gmail.com>
2026-05-11 18:47:32 +0700

    done

---
+++ app/src/main/java/com/wanted/poster/maker/activity_app/adapter/KillerAdapter.kt
@@ -0,0 +1,9 @@
+package com.wanted.poster.maker.activity_app.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class KillerAdapter : RecyclerView.Adapter<KillerAdapter.KillerViewHolder> {
+
+    inner class KillerViewHolder(view : View) : RecyclerView.ViewHolder(view)
+}
---
+++ app/src/main/java/com/wanted/poster/maker/data/model/KillerModel.kt
@@ -0,0 +1,3 @@
+package com.wanted.poster.maker.data.model
+
+data class KillerModel(val tvKiller: String, val ivKiller: Int)
